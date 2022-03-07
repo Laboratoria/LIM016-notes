021b1e5b0005a662175e1ab18834d27759ef94ec
claudiaricce <claudia.ricce@gmail.com>
2022-03-07 12:59:17 -0500

    confirmar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePages from './pages/HomePages';
+import LogInPages from './pages/LogInPages';
+import SingUpPages from './pages/SingUpPages';
+import NotFoundPages from './pages/NotFoundPages';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+  return ( 
+    <BrowserRouter>
+      <Navbar />
+      <Routes> 
+        <Route path="/" element={<LogInPages/>} />
+        <Route path="/singup" element={<SingUpPages/>} />
+        <Route path="/home" element={<HomePages/>} />
+        <Route path="*" element={<NotFoundPages/>} />
+      </Routes>
+    </BrowserRouter>
+  );  
 }
 
-export default App;
+
+
