66313130d43d7af0562b26935580c0841288a327
LindsayMuji <lmelissa.mp@gmail.com>
2022-03-07 09:36:48 -0500

    Create react, Eslint y Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBkUNvRt7tYhGlV3z5nvMhLEG5ylQywhqE',
+  authDomain: 'simple-notes-138ea.firebaseapp.com',
+  projectId: 'simple-notes-138ea',
+  storageBucket: 'simple-notes-138ea.appspot.com',
+  messagingSenderId: '36471898392',
+  appId: '1:36471898392:web:27ee41295fecffa0a11e6d',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+};
