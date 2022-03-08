d0bced37fca97e3394c443cc18cd39d319c35ede
belkisch <belkisch@gmail.com>
2022-03-08 09:31:00 -0500

    pagina de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePages from './pages/HomePages';
+import LogInPages from './pages/login/LogInPages';
+import SingUpPages from './pages/SingUpPages';
+import NotFoundPages from './pages/NotFoundPages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+  return ( 
+    <BrowserRouter>
+      <Routes> 
+        <Route path="/" element={<LogInPages/>} />
+        <Route path="/singup" element={<SingUpPages/>} />
+        <Route path="/home" element={<HomePages/>} />
+        <Route path="*" element={<NotFoundPages/>} />
+      </Routes>
+    </BrowserRouter>
+  );  
 }
 
-export default App;
+
+
---
+++ src/pages/login/LogInPages.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { 
+  Formulario, 
+  Label, 
+  GrupoInput, 
+  Input, 
+  LeyendaError, 
+  IconoValidacion, 
+  ContenedorBotonCentrado,
+  Boton, 
+  MensajeExito, 
+  MensajeError 
+} from './Formularios';
+
+
+export default function LogInPages() {
+  return (
+    <main>
+      <section>
+        <p>LAB NOTES</p>
+        
+        <p>Bienvenid@ a LabNotes </p>
+      </section> 
+      <section>
+        <Formulario action = "">
+          <div>
+            <Label htmlFor="email">Email</Label>
+            <GrupoInput>
+              <Input type="email" placeholder="Email" id="email" />
+              <IconoValidacion icon={faCheckCircle} />
+            </GrupoInput>     
+            <LeyendaError>Lorem ipsum dolor sit amet veniam! Saepe est et aperiam, fugiat soluta ducimus.</LeyendaError>
+          </div>   
+          <div>
+            <Label htmlFor="password">Password</Label>
+            <GrupoInput>
+              <Input type="password" placeholder="password" id="password" />
+              <IconoValidacion icon={faCheckCircle} />
+            </GrupoInput>     
+            <LeyendaError>Lorem ipsum dolor sit amet veniam! Saepe est et aperiam, fugiat soluta ducimus.</LeyendaError>
+          </div> 
+          <p>¿Has Olvidado la Contraseña?</p>
+          <MensajeError>
+            <p>
+              <IconoValidacion icon={faTriangleExclamation}/>
+              <b>Error:</b>Por favor rellene el formulario correctamente
+            </p>
+          </MensajeError> 
+          <ContenedorBotonCentrado>
+            <Boton type="submit">Enviar</Boton>
+            <MensajeExito>Formulario enviado exitosamente</MensajeExito>
+          </ContenedorBotonCentrado> 
+        </Formulario>
+      </section> 
+      <section>
+        <p>O Inicia Session Con:</p>
+        <div style={{fontSize:"2em", color: "black", textAlign: "center"}}>           
+          <FontAwesomeIcon icon={faGoogle} /> {" "}
+        </div>
+        <p>¿No tienes una cuenta?</p>
+      </section>  
+    </main>
+  )
+}
