d07018717392bd50206af68d9379cef6854ac7ff
claudiaricce <claudia.ricce@gmail.com>
2022-03-08 01:13:43 -0500

    style

--- src/pages/LogInPages.jsx
+++ src/pages/LogInPages.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 export default function LogInPages() {
   return (
-    <div>
-      LOG IN
-    </div>
-  )
+        <div id="template" className="templateLogin">
+          <div id="Logo" className="containerLogin">
+            <p className="eslogan">Tomar notas nunca fue tan sencillo</p>
+            <figure><img className="imagenFondo" src="/img/logincontainer.jpg" alt=""></img></figure>
+          </div>
+          <div id="login" className="containerForm">
+            <div className="titulo">
+              <h1 className="title">LABNOTES</h1>
+              <img className="logo2" src="/img/notas.png" alt=""/>
+            </div>
+            <form id="login" className="login">
+            </form>
+          </div>
+        </div>
+    )
 }
