6aaf02c9363cabcc829ec3869a398e7b49c8d31a
LindsayMuji <lmelissa.mp@gmail.com>
2022-03-11 10:59:34 -0500

    direccionar boton singup a vista login

--- src/componentes/Signup.jsx
+++ src/componentes/Signup.jsx
@@ -17,7 +17,7 @@ export const SignUp = () =>{
                 <input type="text" className='name-user' id='log' placeholder='Enter your name'/>
                 <input type="text" className='email' id='log' placeholder='Enter your email'/>
                 <input type="password" className='paswword' id='log' placeholder='Password'/>
-                <button type='submit'className='btn-signup' id='log'>Sign Up</button>
+                <button type='submit'className='btn-signup' id='log'><Link to = '/login'> Sign Up </Link></button>
             </form>
             <p className='optionLogin' id='log'>Do you have an account?<Link to = '/login'>Log In</Link></p>
         </section>
