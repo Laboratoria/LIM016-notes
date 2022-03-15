d87378804191ade2b26bc2a24a6312d1daee0009
Lucia <llopeza@alumno.unsm.edu.pe>
2022-03-15 09:47:45 -0500

    funcion de guardar nota

--- src/componentes/Notes.jsx
+++ src/componentes/Notes.jsx
@@ -2,16 +2,23 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MdDeleteForever, MdCreate } from 'react-icons/md';
 
-export const Notes = () => {
-    return (
-        <div>
-            <div><span>Tittle</span></div>
-            <div>
-                <MdDeleteForever className='delete-icon' size='1.3em'></MdDeleteForever>
-                <MdCreate className='create-icon' size='1.3em'></MdCreate>
+export const Notes = ( props ) => { 
+    // eslint-disable-next-line react/prop-types
+    const templateList = props.notes.map((note) => {
+        // eslint-disable-next-line react/jsx-key
+        let key = 0;
+       return <div key={key+1} className='note-list'>
+            <h3>{note.title}</h3>
+            <p>{note.description}</p>
+            <div className='note-footer'>
+                <h2> {note.date}.</h2>
+                <div>
+                    <MdDeleteForever className='delete-icon' size='1.3em'></MdDeleteForever>
+                    <MdCreate className='create-icon' size='1.3em'></MdCreate>
+                </div>
             </div>
-
-        </div>
-
-    )
+        </div>        
+    });
+    return templateList; 
 };
+
--- src/componentes/Viewnotes.jsx
+++ src/componentes/Viewnotes.jsx
@@ -1,14 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import '../Estilos/Note.scss';
 import {MdLogout, MdSearch} from 'react-icons/md'
 import {AddNotes} from './AddNotes';
 import {Notes} from './Notes';
+import {getNotesByUser} from '../firebase/firestore';
 
 const imgMas = new URL ('../imagenes/mas.png', import.meta.url);
 const imgCategoria = new URL ('../imagenes/categoria.png', import.meta.url);
 const imgRecycle = new URL ('../imagenes/recycle-bin.png', import.meta.url);
 
 export const ViewNotes = () =>{
+
+    const [arrayNotes, setArrayNotes] = useState([]);
+    let tempArrayNotes = [];   
+    useEffect(()=>{
+        getNotesByUser("lucia@gmail.com")
+        .then((response) => {   
+            response.forEach(note => {
+                tempArrayNotes.push(note.data());
+            });
+            setArrayNotes(tempArrayNotes);
+        })
+        .catch((error) => console.log('Error: ', error.message))        
+    },[]);
+
     return (
         <section className='contentNote'>
             <header className='box-header'>
@@ -32,8 +47,8 @@ export const ViewNotes = () =>{
                     <p>Recycle</p>
                 </div>
                 <div className='box-notes'>
-                    <AddNotes></AddNotes>
-                    <Notes></Notes>
+                    <AddNotes arrayNotes={arrayNotes} setArrayNotes={setArrayNotes}></AddNotes>
+                    <Notes notes = {arrayNotes}></Notes>                                    
                 </div>
             </main>
         </section>
