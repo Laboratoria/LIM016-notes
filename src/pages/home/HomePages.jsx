cc476f4c062f9453657b6f1c20ae642a54d45d6b
claudiaricce <claudia.ricce@gmail.com>
2022-03-16 20:34:35 -0500

    add menu

--- src/pages/home/HomePages.jsx
+++ src/pages/home/HomePages.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Principal from "./Principal";
+import Principal from './Principal';
+import Menu from './Menu';
 
 export default function HomePages() {
   return (
     <section className="templatehome" >
       <Principal />
       <main>
-        <Aside />
+        <Menu />
       </main>
     </section>
   )
--- src/pages/login/LogInPages.jsx
+++ src/pages/login/LogInPages.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import Footer from './Footer'
 import { 
   Formulario, 
   Label, 
@@ -17,6 +18,7 @@ import {
 
 export default function LogInPages() {
   return (
+        <>
         <div id="template" className="templateLogin">
           <div id="Logo" className="containerLogin">
             <p className="eslogan">Tomar notas nunca fue tan sencillo</p>
@@ -28,47 +30,49 @@ export default function LogInPages() {
               <img className="logo2" src="/img/notas.png" alt=""/>
             </div>
             <div id="login" className="login">
-            <section>
-        <Formulario action = "">
-          <p>Bienvenid@ a LabNotes </p>
-          <div>
-            <Label htmlFor="email">Email</Label>
-            <GrupoInput>
-              <Input type="email" placeholder="Email" id="email" />
-              <IconoValidacion icon={faCheckCircle} />
-            </GrupoInput>     
-            <LeyendaError>Lorem ipsum dolor sit amet veniam! Saepe est et aperiam, fugiat soluta ducimus.</LeyendaError>
-          </div>   
-          <div>
-            <Label htmlFor="password">Password</Label>
-            <GrupoInput>
-              <Input type="password" placeholder="password" id="password" />
-              <IconoValidacion icon={faCheckCircle} />
-            </GrupoInput>     
-            <LeyendaError>Lorem ipsum dolor sit amet veniam! Saepe est et aperiam, fugiat soluta ducimus.</LeyendaError>
-          </div> 
-          <p>¿Has Olvidado la Contraseña?</p>
-          <MensajeError>
-            <p>
-              <IconoValidacion icon={faTriangleExclamation}/>
-              <b>Error:</b>Por favor rellene el formulario correctamente
-            </p>
-          </MensajeError> 
-          <ContenedorBotonCentrado>
-            <Boton type="submit">Enviar</Boton>
-            <MensajeExito>Formulario enviado exitosamente</MensajeExito>
-          </ContenedorBotonCentrado> 
-        </Formulario>
-      </section> 
-      <section>
-        <p>O Inicia Session Con:</p>
-        <div style={{fontSize:"2em", color: "black", textAlign: "center"}}>           
-          <FontAwesomeIcon icon={faGoogle} /> {" "}
-        </div>
-        <p>¿No tienes una cuenta?</p>
-      </section>
+              <section>
+                <Formulario action = "">
+                  <p>Bienvenid@ a LabNotes </p>
+                  <div>
+                    <Label htmlFor="email">Email</Label>
+                    <GrupoInput>
+                      <Input type="email" placeholder="Email" id="email" />
+                      <IconoValidacion icon={faCheckCircle} />
+                    </GrupoInput>     
+                    <LeyendaError>Lorem ipsum dolor sit amet veniam! Saepe est et aperiam, fugiat soluta ducimus.</LeyendaError>
+                  </div>   
+                  <div>
+                    <Label htmlFor="password">Password</Label>
+                    <GrupoInput>
+                      <Input type="password" placeholder="password" id="password" />
+                      <IconoValidacion icon={faCheckCircle} />
+                    </GrupoInput>     
+                    <LeyendaError>Lorem ipsum dolor sit amet veniam! Saepe est et aperiam, fugiat soluta ducimus.</LeyendaError>
+                  </div> 
+                  <p>¿Has Olvidado la Contraseña?</p>
+                  <MensajeError>
+                    <p>
+                      <IconoValidacion icon={faTriangleExclamation}/>
+                      <b>Error:</b>Por favor rellene el formulario correctamente
+                    </p>
+                  </MensajeError> 
+                  <ContenedorBotonCentrado>
+                    <Boton type="submit">Enviar</Boton>
+                    <MensajeExito>Formulario enviado exitosamente</MensajeExito>
+                  </ContenedorBotonCentrado> 
+                </Formulario>
+              </section> 
+              <section>
+                <p>O Inicia Session Con:</p>
+                <div style={{fontSize:"2em", color: "black", textAlign: "center"}}>           
+                  <FontAwesomeIcon icon={faGoogle} /> {" "}
+                </div>
+                <p>¿No tienes una cuenta?</p>
+              </section>
             </div>
           </div>
         </div>
+        < Footer />
+      </>
     )
 }
