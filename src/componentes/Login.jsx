8b1b6254a8f809b00f48e7350ee287b8f6117e0c
LindsayMuji <lmelissa.mp@gmail.com>
2022-03-28 17:08:06 -0500

    merge ramas

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../Estilos/register.scss';
-import {signIn, singInGoogle } from '../firebase/auth';
-import {FcGoogle} from 'react-icons/fc';
- 
+import { signIn, singInGoogle } from '../firebase/auth';
+import { FcGoogle } from 'react-icons/fc';
+
 
 
 
 import imgLogoNotes from '../imagenes/logoNotes.jpg';
-import imgIconoGo from '../imagenes/iconoGo.png';
 import imgIconoFb from '../imagenes/iconoFb.png';
 
 export { imgLogoNotes }
@@ -17,9 +16,9 @@ export const LogIn = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
-    const navigate = useNavigate ();
+    const navigate = useNavigate();
 
-    
+
     const handleGoogleSubmit = (e) => singInGoogle();
     const handleEmail = (e) => setEmail(e.target.value);
     const handlePassword = (e) => setPassword(e.target.value);
@@ -29,21 +28,21 @@ export const LogIn = () => {
             const user = await signIn(email, password)
             const currentUser = user.user.uid;
             navigate('/viewnotes', user.user.id)
-        } catch(error) {
-            setError('Your email or password are wrong');  
-        }      
+        } catch (error) {
+            setError('Your email or password are wrong');
+        }
     };
 
-    
+
     return (
         <section className='box-login'>
-            <div className='boxWelcome'> 
+            <div className='boxWelcome'>
                 <h3 className='welcome' >Welcome To</h3>
                 <div className='box-logo'>
                     <img src={imgLogoNotes} alt="" className='img-logo'></img>
                     <h1 className='name-notes'>Simple Notes</h1>
                 </div>
-            </div>          
+            </div>
             {error && <p className='error'>{error}</p>}
             <form action="" className='box-form' onSubmit={handleSubmit}>
                 <div className='boxInput'>
@@ -53,18 +52,11 @@ export const LogIn = () => {
                 </div>
                 <p className='textOption'>or log in with:</p>
                 <div className='box-fb-go'>
-                    <img src={imgIconoGo} alt="" className='img-fb' ></img>
+                    <FcGoogle onClick={handleGoogleSubmit} className='img-go' size='2em' id='log'></FcGoogle>
                     <img src={imgIconoFb} alt="" className='img-go' ></img>
                 </div>
                 <p className='optionSignUp'>Don´t you have an account?<Link to='/signup'> Sign Up </Link></p>
             </form>
-
-            <p className='textOption' id='log'>or log in with:</p>
-            <div className='box-fb-go' id='log'>
-                <FcGoogle onClick= {handleGoogleSubmit} className='img-go' size='2em' id='log'></FcGoogle>
-                <img src={imgIconoFb} alt="" className='img-fb' id='log'></img>
-            </div>
-            <p className='optionSignUp' id='log'>Don´t you have an account?<Link to='/signup'> Sign Up </Link></p>
         </section>
     );
 };
